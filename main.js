e556fd248b2b2ec9230e546deb07a62780d409bf
ailin torre val <aili0057@stud.kea.dk>
2022-04-04 16:14:02 +0200

    monday

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 import "./sass/style.scss";
 
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+const key = "AIzaSyCv8E4cyKmeVNQtrLEnzrIMh9QnnOaKkT4";
+const url = encodeURIComponent("https://www.infobae.com/america/");
+async function getSpeedData() {
+  const result = await fetch(
+    `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${url}&key=${key}`
+  );
+  const data = await result.json();
+  console.log("lighthouse", data);
+}
+getSpeedData();
+
+const url1 =
+  "https://kea-alt-del.dk/websitecarbon/site/?url=https://www.infobae.com/america/";
+
+function getGreenData() {
+  fetch(url1)
+    .then((res) => res.json())
+
+    .then(gotGreenData);
+}
+function gotGreenData(data) {
+  console.log("green", data);
+}
+getGreenData();
+
+
+//dataArr[1]["unminified-javascript"];
